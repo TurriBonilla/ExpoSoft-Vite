43ea95dccfb762433eb54cf78712f095832dfbd5
LuisFerSk <lcampomontero@gmail.com>
2022-03-15 15:59:12 -0500

    change of file extension from .jsx to tsx

---
+++ src/const/dataFormRegister.tsx
@@ -0,0 +1,33 @@
+const dataFormRegister = [
+	{
+		name: 'name',
+		placeholder: 'Nombre',
+		type: 'text',
+	},
+	{
+		name: 'surname',
+		placeholder: 'Apellidos',
+		type: 'text',
+	},
+	{
+		name: 'email',
+		placeholder: 'Correo electrónico',
+		type: 'email',
+	},
+	{
+		name: 'confirmEmail',
+		placeholder: 'Confirma tu correo',
+		type: 'email',
+	},
+	{
+		name: 'password',
+		placeholder: 'Contraseña',
+		type: 'password',
+	},
+	{
+		name: 'confirmPassword',
+		placeholder: 'Confirma tu contraseña',
+		type: 'password',
+	},
+]
+export default dataFormRegister
