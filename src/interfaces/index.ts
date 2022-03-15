d5c35c6e321fa37149eded15e7cb2f43ab42da67
LuisFerSk <lcampomontero@gmail.com>
2022-03-15 16:00:45 -0500

    update interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,41 +1,63 @@
-import { Dispatch, InputHTMLAttributes, MouseEvent, SetStateAction } from 'react'
+import {
+	Dispatch,
+	InputHTMLAttributes,
+	MouseEventHandler,
+	SetStateAction,
+} from 'react'
 
-export interface ICredentials {
-  email: string
-  password: string
+export interface CredentialsInterface {
+	email: string
+	password: string
 }
 
-export interface IRegister {
-  name: string
-  surname: string
-  email: string
-  confirmEmail: string
-  password: string
-  confirmPassword: string
+export interface RegisterInterface {
+	name: string
+	surname: string
+	email: string
+	confirmEmail: string
+	password: string
+	confirmPassword: string
 }
 
 export interface SidebarProps {
-  showMenu: boolean
+	showMenu: boolean
 }
 
-export interface CardHelpProps {
-  title: string
-  icon: JSX.Element
-  isActive: boolean
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+export interface OnClickInterface<T> {
+	onClick: MouseEventHandler<T>
+}
+
+export interface CardHelpProps
+	extends OnClickInterface<HTMLButtonElement>,
+		IsActiveInterface {
+	title: string
+	icon: JSX.Element
 }
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  setState: Dispatch<SetStateAction<any>>
-  name: any
+	setState: Dispatch<SetStateAction<any>>
+	name: string
 }
 
-export interface IFaq {
-  faq: string,
-  body: string,
-  show: boolean
+export interface FaqInterface {
+	faq: string
+	body: string
+	show: boolean
 }
 
-export interface IResultModule {
-  title: string
+export interface ResultModuleInterface {
+	title: string
+}
+
+export interface ColorInterface { 
+	color: 'primary' | 'secondary' | 'error' | 'alert' | 'success'
+}
+
+export interface FaqsProps {
+	faq: string
+	body: string
+}
+
+export interface IsActiveInterface {
+	isActive: boolean
 }
