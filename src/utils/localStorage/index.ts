468f17226b7852816b1e0b9d80db504b45e435f0
TurriBonilla <turridev@gmail.com>
2022-03-18 17:47:40 -0500

    Hook localStorage add

---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export const useLocalStorage = (key: string, initValue?: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+
+      return item ? JSON.parse(item) : initValue
+    } catch (e) {
+      return initValue
+    }
+  })
+
+  const setValue = (value: any) => {
+    try {
+      setStoredValue(value)
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const removeItem = (key: string) => {
+    try {
+      window.localStorage.removeItem(key)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return [storedValue, setValue, removeItem]
+}
