845cb1e1f6c4de0c074a7f1e64bf9f87d1bf3d6d
TurriBonilla <turridev@gmail.com>
2022-03-17 20:32:33 -0500

    Fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: './build'
+  }
 })
